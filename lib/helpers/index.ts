ebfb89aacf3940ddef86dabcca8fde0c13b200a9
Djawad <dev.javad.mousavian@gmail.com>
2026-02-11 19:43:18 +0330

    feat(helper) : add get space between 2 date (getRemainingTime) and format persian date helpers

--- lib/helpers/index.ts
+++ lib/helpers/index.ts
@@ -1,3 +1,29 @@
 export function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export function getRemainingTime(from: Date, to: Date) {
+  const diffMs = Math.max(to.getTime() - from.getTime(), 0);
+
+  const totalMinutes = Math.floor(diffMs / (1000 * 60));
+  const totalHours = Math.floor(totalMinutes / 60);
+
+  const days = Math.floor(totalHours / 24);
+  const hours = totalHours % 24;
+
+  return { days, hours };
+}
+
+type PersianDate = {
+  day?: 'numeric';
+  month: 'numeric' | 'narrow' | 'long';
+  year: '2-digit' | 'numeric' | false;
+};
+
+export function formatPersianDate(date: Date, { day = 'numeric', month, year }: PersianDate) {
+  return new Intl.DateTimeFormat('fa-IR-u-nu-latn', {
+    day,
+    month,
+    ...(year && { year }),
+  }).format(date);
+}
