65b9b31e928fa46878a4a24b92450875f3fe27b5
Djawad <dev.javad.mousavian@gmail.com>
2026-03-21 16:54:05 +0330

    fix(auth) : fix security bug . if user was not login redirect him to login page

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,12 @@
+import { redirect } from 'next/navigation';
+
+import { getCurrentUser } from '@/lib/auth/session';
+
 import BottomNavigation from '@/components/common/BottomNavigation';
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
+  const currentUser = await getCurrentUser();
+  if (!currentUser?.id) redirect('/login');
   return (
     <>
       {children}
